0158037ea6e46e938d48303a37cb19be1996e2e7
James Kehan <jxk5891@psu.edu>
2023-04-25 20:01:27 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ECC
+
+go 1.20
+
+require gitlab.com/elktree/ecc v0.0.0-20190314182556-d86af7b58391
---
+++ main.go
@@ -0,0 +1,29 @@
+// I found this sample of ECC using elktree's "GenerateKeys", "Encrypt" and "Decrypt" functions, I feel as if this page had the most simple example code-wise for me to understand ECC, I further edited the code to allow for user input
+// https://pkg.go.dev/gitlab.com/elktree/ecc
+// Only problem is it only encrypts first word
+package main
+
+import (
+	"crypto/elliptic"
+	"fmt"
+	"gitlab.com/elktree/ecc"
+)
+
+func main() {
+	// Create keys using his GenerateKeys function within ECC call
+	pub, priv, _ := ecc.GenerateKeys(elliptic.P521())
+
+	// Asks for user input for plaintext
+	fmt.Println("Enter Your Plaintext: ")
+	var plaintext string
+	fmt.Scanln(&plaintext)
+
+	// Uses a public key to complete encrypt function
+	encrypted, _ := pub.Encrypt([]byte(plaintext))
+	fmt.Println(len(encrypted))
+
+	// Uses private key to complete decrypt function
+	decrypted, _ := priv.Decrypt(encrypted)
+	fmt.Println(string(decrypted))
+
+}
